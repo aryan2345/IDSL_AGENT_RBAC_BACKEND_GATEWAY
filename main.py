b26fc6cf90785fe45a6e914c42f2500af754a956
parth.kathiria <parth.kathiria169@gmail.com>
2025-07-01 17:58:31 -0400

    updates on the integration

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ app = FastAPI()
 # CORS middleware for frontend (React) integration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["http://localhost:5179", "http://localhost:5180"],
+    allow_credentials= True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
